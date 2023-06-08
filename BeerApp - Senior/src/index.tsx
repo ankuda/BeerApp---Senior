d39da22f4ea2f202f2fc278a43c8b03ea7c318cc
ankuda <ankudakirill@gmail.com>
2023-06-08 20:28:20 +0200

    add PWA, offline

--- BeerApp - Senior/src/index.tsx
+++ BeerApp - Senior/src/index.tsx
@@ -4,6 +4,9 @@ import Router from './router';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './styles/theme';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+
 import './styles/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
@@ -15,3 +18,13 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
